64b53ec818d4c45e48ff75b8e093f2407b700840
Layssa <layssa.lima166@gmail.com>
2022-03-01 15:13:03 -0300

    delete: whitespaces

--- pub-app/server.js
+++ pub-app/server.js
@@ -2,8 +2,6 @@ const express = require("express");
 
 const { router } = require("./src/routers");
 
-
-
 const app = express();
 
 app.use(express.json());
@@ -14,6 +12,8 @@ app.use(router);
 app.listen(3030, () => {
   console.log("Pub server runing at port", 3030);
 });
+
+
 
 
 
